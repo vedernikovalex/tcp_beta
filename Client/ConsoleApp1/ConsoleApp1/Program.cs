821b8ba66c8d4cde0dcfdc7611906026e08eefd0
Alexander Vedernikov <vedurnikof@gmail.com>
2022-11-30 20:23:52 +0100

    initial commit

---
+++ Client/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        static string ip_address = "127.0.0.1";
+        static int port = 7777;
+
+        static void Main(string[] args)
+        {
+            TcpClient tcpClient = new TcpClient();
+            tcpClient.Connect(ip_address, port);
+            Console.WriteLine("connected!");
+            NetworkStream ns = tcpClient.GetStream();
+
+            Thread thread = new Thread(o => ReceiveData((TcpClient)o));
+            thread.Start(tcpClient);
+
+            string s;
+            while (!string.IsNullOrEmpty((s = Console.ReadLine())))
+            {
+                byte[] buffer = Encoding.ASCII.GetBytes(s);
+                ns.Write(buffer, 0, buffer.Length);
+            }
+
+            tcpClient.Client.Shutdown(SocketShutdown.Send);
+            thread.Join();
+            ns.Close();
+            tcpClient.Close();
+            Console.WriteLine("disconnected!");
+            Console.ReadKey();
+        }
+
+        static void ReceiveData(TcpClient client)
+        {
+            NetworkStream ns = client.GetStream();
+            byte[] receivedBytes = new byte[1024];
+            int byte_count;
+
+            while ((byte_count = ns.Read(receivedBytes, 0, receivedBytes.Length)) > 0)
+            {
+                Console.Write(Encoding.ASCII.GetString(receivedBytes, 0, byte_count));
+            }
+        }
+    }
+}
---
+++ Tcp_Beta/TCP_Beta/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCP_Beta
+{
+    class Program
+    {
+        static Dictionary<int, TcpClient> client_list = new Dictionary<int, TcpClient>();
+        static object locker = new object();
+
+        static void Main(string[] args)
+        {
+            int count = 1;
+
+            TcpListener server = new TcpListener(IPAddress.Any, 7777);
+            server.Start();
+
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                lock (locker) client_list.Add(count, client);
+                Console.WriteLine("Connection established!");
+
+                Thread thread = new Thread(handle);
+                thread.Start(count);
+                count++;
+            }
+
+        }
+
+        public static void handle(object obj)
+        {
+            int id = (int)obj;
+            TcpClient client;
+            lock (locker) client = client_list[id];
+
+            while (true)
+            {
+                NetworkStream stream = client.GetStream();
+                byte[] buffer = new byte[1024];
+                int byte_count = stream.Read(buffer, 0, buffer.Length);
+
+                if (byte_count == 0)
+                {
+                    break;
+                }
+
+                string data = Encoding.ASCII.GetString(buffer, 0, byte_count);
+                broadcast(data);
+                Console.WriteLine(data);
+            }
+
+            lock (locker) client_list.Remove(id);
+            client.Client.Shutdown(SocketShutdown.Both);
+            client.Close();
+        }
+
+        public static void broadcast(string data)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(data + Environment.NewLine);
+            lock (locker)
+            {
+                foreach(TcpClient client in client_list.Values)
+                {
+                    NetworkStream stream = client.GetStream();
+                    stream.Write(buffer, 0, buffer.Length);
+                }
+            }
+        }
+    }
+}
